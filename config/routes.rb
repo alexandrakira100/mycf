9bbc9bfb3a377c09e93620a37751393822d03d14
Tim Cheah <tim.cheah.89@gmail.com>
2018-06-06 12:28:50 +0900

    allowed fund managers to add members to a fund

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
   root to: 'funds#index'
 
   resources :funds do
     resources :fund_items, only: [:new, :create, :edit, :update, :destroy]
     resources :memberships, only: [:new, :create, :destroy]
-    resources :messages, only: [:create]
+    resources :messages, only: [:create ]
   end
 
  resources :coins, only: [:create, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  end
+end
+
