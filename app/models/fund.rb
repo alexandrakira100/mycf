bfdddf1170d269c66a94958fc8e79012c59417c5
Tim Cheah <tim.cheah.89@gmail.com>
2018-06-06 16:32:18 +0900

    ultra-basic css so that the fundspace is not completely messed up and added functionality so that only the fund owner can add investors

--- app/models/fund.rb
+++ app/models/fund.rb
@@ -1,4 +1,4 @@
-class Fund < ApplicationRecord
+  class Fund < ApplicationRecord
   has_many :fund_items
   validates :name, presence: true
   belongs_to :owner, class_name: :User, foreign_key: :owner_id
