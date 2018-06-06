9e468660b5e9fe702bc29bb6b2ef3e71aea21c17
Alexandra Doll <alexandra.doll100@gmail.com>
2018-06-06 14:25:15 +0900

    coin value in usd

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,12 @@
+// require 'json';
+// require 'open-uri'
+
 import "bootstrap";
+
+// url = 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD'
+// crypto_initial = open(url).read
+// crypto = JSON.parse(crypto_initial)
+
+// puts "#{Price['USD']}"
+
+// console.log(data)
