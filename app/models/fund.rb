78bb36ea15038245ce0e5b6d060dd3148881b5dc
Alexandra Doll <alexandra.doll100@gmail.com>
2018-06-06 16:19:26 +0900

    total fund value

--- app/models/fund.rb
+++ app/models/fund.rb
@@ -5,4 +5,15 @@ class Fund < ApplicationRecord
   has_many :memberships
   has_many :members, through: :memberships, source: :user, class_name: :User
   has_many :messages, dependent: :destroy
+
+  def total_fund_value
+    sum = 0
+
+    fund_items.each do |fund_item|
+      fund_value = (fund_item.coin.price_in_cents / 100.00) * fund_item.quantity
+      sum += fund_value
+    end
+
+    sum
+  end
 end
