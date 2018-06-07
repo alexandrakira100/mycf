edf9a4503559fc98ec9309ce24dc25f1a0afe6ef
Hiroaki <hiroaki.fukuoka@gmail.com>
2018-06-07 19:39:28 +0900

    input exact amount of allocation and shows percentage

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,5 +1,5 @@
 class Membership < ApplicationRecord
   belongs_to :fund
   belongs_to :user
-  validates :allocation_share, numericality: { greater_than: 0, less_than: 100 }
+  validates :allocation_share, numericality: { greater_than: 0 }
 end
