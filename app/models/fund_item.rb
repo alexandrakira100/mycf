bd288621f77a46d71dbdd5a4da6c016cf940d1af
Tim Cheah <tim.cheah.89@gmail.com>
2018-06-14 18:32:05 +0900

    added change column to portfolio table

--- app/models/fund_item.rb
+++ app/models/fund_item.rb
@@ -4,5 +4,10 @@ class FundItem < ApplicationRecord
   validates :quantity, presence: true, numericality: { greater_than: 0 }
   validates :coin_purchase_price_in_cents, presence: true, numericality: { greater_than: 0 }
   monetize :coin_purchase_price_in_cents, as: :coin_purchase_price
+
+  def change
+    (((self.coin.price_in_cents - self.coin_purchase_price_in_cents) * self.quantity) / (self.coin_purchase_price_in_cents / 100 * self.quantity)).round(1)
+  end
+
 end
 
